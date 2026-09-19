a813b728d8d1f39498a349f90389312e21b70287
Fatema  M. Shakir <158505985+Fatemaaa25@users.noreply.github.com>
2026-09-19 21:14:04 +0530

    Refactor server.js to export app module
    Removed server listening log and exported app module.

--- Backend/server.js
+++ Backend/server.js
@@ -4,6 +4,4 @@ const connectToDB = require("./src/config/db");
 const generateInterviewReport = require("./src/services/ai.service");
 
 connectToDB();
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
-});
+module.exports = app;
