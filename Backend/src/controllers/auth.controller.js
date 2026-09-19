b7b27707145d652eaf00e37e5acc7a7c6aff6434
Fatemaaa25 <fatema.shakir03@gmail.com>
2026-09-19 23:21:18 +0530

    cookie error handling

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -82,7 +82,12 @@ async function loginUserController(req, res) {
     { expiresIn: "1d" },
   );
 
-  res.cookie("token", token);
+  res.cookie("token", token, {
+    httpOnly: true,
+    secure: true,
+    sameSite: "none",
+    path: "/",
+  });
   res.status(200).json({
     message: "User loggedIn successfully.",
     user: {
